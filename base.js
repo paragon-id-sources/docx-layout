791d8d8af935b0283ec7346a4f8de0e44de24260
paragon-id-sources <100785900+paragon-id-sources@users.noreply.github.com>
2022-03-04 10:35:34 +0000

    Update base.js

--- base.js
+++ base.js
@@ -1,119 +1,106 @@
-﻿function SanitateAsID(id) {
-    return String(id).replace(" ", "-")
-}
-function GetHeaders() {
-    return $(":header").toArray();
-}
+function GetHeaders() { return $(":header").toArray(); }
+function GetHeaderIndentLevel(header) { return String(header).replace(/^\D+/g, ''); }
+function GenerateGUID() { return "nav-" + ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16) ); }
+function ScrollToID(id) { document.getElementById(String(id).replace(" ", "-")).scrollIntoView(); }
 
-function GetHeaderIndentLevel(header) {
-    return String(header).replace(/^\D+/g, '');
-}
-
-function GetTree() {
-    var testdata = []
+function GenerateJSON() {
+    var headerData = []
     var headers = GetHeaders();
     headers.forEach(element => {
         let obj = {
             text: element.textContent,
-            rank: GetHeaderIndentLevel(element.tagName)
+            id: element.id,
+            guid: GenerateGUID(),
+            level: GetHeaderIndentLevel(element.tagName)
         }
-        testdata.push(obj)
+        headerData.push(obj)
     });
 
-    return getTree(testdata)
-
+    return getTree(headerData)
 }
 
-function ScrollToID(id) {
-    id = String(id).replace(" ", "-")
-    var x = document.getElementById(SanitateAsID(id));
-    x.scrollIntoView();
+function pushToTree(obj, tree) {
+    tree[obj.level - 1].children = tree[obj.level - 1].children || [];
+    tree[obj.level - 1].children.push(obj);
+    tree[obj.level] = obj;
+    return tree
 }
+
 function getTree(array) {
-    var rootTree = [{ text: "Headers", rank: "0" }]
+    var rootTree = [{ text: "Headers", level: "0", guid: "base" }]
     var levels = [{}];
+    var tmpLevel = 0
     array.forEach(function (o) {
-        levels.length = o.rank;
-        levels[o.rank - 1].children = levels[o.rank - 1].children || [];
-        levels[o.rank - 1].children.push(o);
-        levels[o.rank] = o;
+        levels.length = o.level;
+
+        if(o.level - tmpLevel > 1) {
+            // o.level = String(parseInt(o.level) - (parseInt(tmpLevel) + 1))
+            for (let index = tmpLevel; index < parseInt(o.level) + 1; index++) {
+                let obj = {
+                    text: "undefined",
+                    id: "undefined-" + GenerateGUID(),
+                    guid: GenerateGUID(),
+                    level: GetHeaderIndentLevel(index)
+                }
+                levels = pushToTree(obj, levels)                        
+            }
+        }
+        levels = pushToTree(o, levels)
+
+        tmpLevel = o.level
     });
     rootTree[0].children = levels[0].children
+
+    SetParentIDs(rootTree[0]);
+
     return rootTree[0];
 }
 
-function AddDropdownEvents() {
-    let arrow = document.querySelectorAll(".arrow");
-    for (var i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener("click", (e) => {
-            let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-            arrowParent.classList.toggle("showMenu");
-        });
-    }
+// Recursively set a GUID on every single document of its parent 
+function SetParentIDs(array) {
+    if(array.guid == null) { array.guid = "base"}
+    array.children.forEach(function (child) {
+        child.parent = array.guid
+        if(child.children != null) { SetParentIDs(child); }
+    })
 }
 
-var parseJsonAsHTMLTree = function (jsn) {
-    result = ""
-    if (jsn.text) {
+function parseJSONToNav(jsn) {
+    var result = ""
 
-        if (jsn.rank == 2) {
-            return ``
-        }
-
-        if (jsn.children == null && jsn.rank == 1) {
-            result += ""
-                + `<li>`
-                + `<a onclick="ScrollToID('${jsn.text.toLowerCase()}')">`
-                + `<i class='bx bx-grid-alt' ></i>`
-                + `<span class="link_name link-jumper" onclick="ScrollToID('${jsn.text.toLowerCase()}')">${jsn.text}</span>`
-                + `</a>`
-                + `</li>`
-        }
-
-        if (jsn.children != null) {
-            if (parseInt(jsn.children.length) && jsn.rank <= 2) {
-                result += ""
-                    + `<li>`
-                    + `<div class="icon-link">`
-                    + `<a >`
-                    + `<i class="bx bx-box"></i>`
-                    + `<span class="link_name link-jumper" onclick="ScrollToID('${jsn.text.toLowerCase()}')">${jsn.text}</span>`
-                    + `</a><i class="bx bxs-chevron-down arrow"></i></div>`
-                    + `<ul class="sub-menu">`
-                    + `<li>`
-                    + `<a class="link_name">${jsn.text}</a>`
-                    + `</li>`
-
-                for (var i in jsn.children) {
-                    if (jsn.children[i].rank == 2)
-                        result += `<li class="sub-child"><a class="link-jumper" onclick="ScrollToID('${jsn.children[i].text.toLowerCase()}')">${jsn.children[i].text}</a></li>`
-
-                }
-
-                result += `</ul></li>`
-
-                for (var i in jsn.children) {
-                    result += parseJsonAsHTMLTree(jsn.children[i]);
-                }
-            }
-        }
+    if(jsn.children == null) { 
+        return `<li><a id="${jsn.guid}"class="link-dark rounded">${jsn.text}</a></li>` 
     }
+
+    jsn.children.forEach(function(object) {
+        if(object.children == null) {
+            if(object.parent == "base") {
+                result += `<li><button onclick="ScrollToID('${object.id}')" class="btn btn-toggle-no-icon"ata-bs-toggle="collapse" aria-expanded="false"> ${object.text}</button></li>`
+                return result;
+            }
+            result += `<li><a onclick="ScrollToID('${object.id}')" class="link-dark rounded">${object.text}</a></li>` 
+            return result; 
+        }
+
+        result += `<li class="mb-1">`;
+        result += `<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#${object.guid}-collapse" aria-expanded="false">`;
+        result += `${object.text}`;
+        result += `</button>`;
+        result += `<div class="collapse" id="${object.guid}-collapse">`;
+        result += `<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">`;
+        result += parseJSONToNav(object)
+        result += `</ul>`
+        result += `</div>`
+        result += `</li>`
+    });
+
     return result;
 }
 
 window.onload = function () {
-    var treeRaw = GetTree()
-    var result = '<div class=\"logo-details\"></div><ul class=\"nav-links\">';
+    console.log(GenerateJSON())
+    document.getElementById('navigator').innerHTML = parseJSONToNav(GenerateJSON());
 
-    result += parseJsonAsHTMLTree(treeRaw);
-
-    result += '</ul></div>';
-
-    document.getElementById('result').innerHTML = result;
-
-    AddDropdownEvents();
-
-    var results = document.getElementById("result")
-    var navLinks = results.getElementsByTagName("ul")[0]
-    navLinks.removeChild(navLinks.getElementsByTagName('li')[0]);
 };
+
+
