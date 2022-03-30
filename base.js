661ac7016d6d8ef3b2cd101a5b3535f589177fa6
paragon-id-sources <100785900+paragon-id-sources@users.noreply.github.com>
2022-03-30 16:16:45 +0100

    Update base.js

--- base.js
+++ base.js
@@ -1,50 +1,28 @@
-
 try {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { mermaid.initialize({ 'theme': 'dark' }); }
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { mermaid.initialize({ 'theme': 'dark' }); }
 } catch (error) {
-  console.error("Failed to theme mermaid.")
-  console.error(error)
+    console.error("Failed to theme mermaid.")
+    console.error(error)
 }
-
-var favicon_url = "https://i.imgur.com/aaEnmwt.png";
-var jsLinks = [ ]
-
-
-function AddNewScriptTags() {
-    jsLinks.forEach(link => {
-        let s = document.createElement("script");
-        s.type = "text/javascript";
-        s.src = link;
-
-        document.head.appendChild(s);
-    });
-
-}
-
 
 function GetHeaders() { return document.querySelectorAll("h1, h2, h3, h4, h5, h6"); }
 function GetHeaderIndentLevel(header) { return String(header).replace(/^\D+/g, ''); }
-function GenerateGUID() { return "nav-" + ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16) ); }
+function GenerateGUID() { return "nav-" + ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)); }
 function ScrollToID(id) { document.getElementById(String(id).replace(" ", "-")).scrollIntoView(); }
 
-
-function changeFavicon(src) {
-    document.head = document.head || document.getElementsByTagName('head')[0];
-     let link = document.createElement('link'),
-         oldLink = document.getElementById('dynamic-favicon');
-     link.id = 'dynamic-favicon';
-     link.rel = 'shortcut icon';
-     link.href = src;
-     if (oldLink) {
-      document.head.removeChild(oldLink);
-     }
-     document.head.appendChild(link);
+function SetTheme(theme) {
+    try {
+        document.documentElement.className = theme
+        localStorage.setItem('theme', theme);
+    } catch (error) {
+        console.error(`Failed to theme document with theme '${theme}'.`)
+        console.error(error)
+    }
 }
 
-
 function GenerateJSON() {
-    var headerData = [];
-    var headers = GetHeaders();
+    let headerData = [];
+    let headers = GetHeaders();
 
     // Iterate over each `h` tag in the html
     // Assign text, id, guid and level
@@ -81,18 +59,18 @@ function pushToTree(obj, tree) {
 // Generate tree children
 function getTree(array) {
     // Create a root object to declare where the tree begins
-    var rootTree = [{ text: "Headers", level: "0", guid: "base" }];
-    var levels = [{}];
+    let rootTree = [{ text: "Headers", level: "0", guid: "base" }];
+    let levels = [{}];
 
     // temp level to compare previous iteration over the last one
     // we use this to handle improper headers, e.g `h1 -> h2 -> h4`
     // there is a h3 missing, which would cascade through the children in the tree
-    var tmpLevel = 0
+    let tmpLevel = 0
     array.forEach(function (o) {
         levels.length = o.level;
 
         // If current object has a level difference of previous object greater than one
-        if(o.level - tmpLevel > 1) {
+        if (o.level - tmpLevel > 1) {
             // o.level = String(parseInt(o.level) - (parseInt(tmpLevel) + 1))
 
             // create new blank level for each level which is missing
@@ -103,7 +81,7 @@ function getTree(array) {
                     guid: GenerateGUID(),
                     level: GetHeaderIndentLevel(index)
                 }
-                levels = pushToTree(obj, levels);                     
+                levels = pushToTree(obj, levels);
             }
         }
         levels = pushToTree(o, levels);
@@ -124,32 +102,32 @@ function getTree(array) {
 // We use the object parent guid to construct the sidebar and properly link dropdown toggles
 // This allows us to know level, and parent, without having to compare to previous elements   
 function SetParentIDs(array) {
-    if(array.guid == null) { array.guid = "base"}
+    if (array.guid == null) { array.guid = "base" }
     array.children.forEach(function (child) {
         child.parent = array.guid
-        if(child.children != null) { SetParentIDs(child); }
+        if (child.children != null) { SetParentIDs(child); }
     })
 };
 
 function parseJSONToNav(jsn) {
-    var result = "";
+    let result = "";
 
-    if(jsn.children == null && jsn.text == null) {  return `` }
+    if (jsn.children == null && jsn.text == null) { return `` }
 
     // Iterate over all children in object
-    jsn.children.forEach(function(object) {
+    jsn.children.forEach(function (object) {
 
         // If object has no children
         // Create a clickable link to section, but do not make a dropdown as ther are no children
-        if(object.children == null) {
-            if(object.parent == "base") {
+        if (object.children == null) {
+            if (object.parent == "base") {
                 // if part of base make a button, to differentiate between normal item with no children and a h1 heading
                 result += `<li><button onclick="ScrollToID('${object.id}')" class="btn btn-sidebar btn-toggle-no-icon"ata-bs-toggle="collapse" aria-expanded="false"> ${object.text}</button></li>`
-                result += `<li class="border-top my-3"></li>`
+                result += `<div class="cst-border"></div>`
                 return result;
             }
-            result += `<li><a onclick="ScrollToID('${object.id}')" class="link-dark rounded">${object.text}</a></li>` 
-            return result; 
+            result += `<li><a onclick="ScrollToID('${object.id}')" class="link-dark rounded">${object.text}</a></li>`
+            return result;
         };
 
 
@@ -174,26 +152,26 @@ function parseJSONToNav(jsn) {
         result += `</li>`
 
         // Add seperator to each root item in nav bar
-        result += object.level == 1 ? `<li class="border-top my-3"></li>` : ``
+        result += object.level == 1 ? `<li class="cst-border"></li>` : ``
     });
 
     return result;
 };
 
 function ExpandAllNavItems() {
-    var sidebar = document.getElementById("section-sidebar")
-    var buttons = sidebar.getElementsByClassName("collapse")
+    let sidebar = document.getElementById("section-sidebar")
+    let buttons = sidebar.getElementsByClassName("collapse");
 
-    for(i = 0; i < buttons.length; i++) {
+    for (i = 0; i < buttons.length; i++) {
         buttons[i].classList.add("show")
     }
 }
 
 function CollapseAllNavItems() {
-    var sidebar = document.getElementById("section-sidebar")
-    var buttons = sidebar.getElementsByClassName("collapse")
+    let sidebar = document.getElementById("section-sidebar")
+    let buttons = sidebar.getElementsByClassName("collapse")
 
-    for(i = 0; i < buttons.length; i++) {
+    for (i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove("show")
     }
 }
@@ -202,25 +180,22 @@ function CollapseAllNavItems() {
 window.onload = function () {
     try {
         let footnoteDiv = document.getElementsByClassName("footnotes")
-        if (footnoteDiv.length > 0 ) {
-            footnote = footnoteDiv[footnoteDiv.length - 1]
-            footnote.insertAdjacentHTML('beforebegin','<h1 id="footnote-section-div">Footnotes</h1>')
+        if (footnoteDiv.length > 0) {
+            let footnote = footnoteDiv[footnoteDiv.length - 1]
+            footnote.insertAdjacentHTML('beforebegin', '<h1 id="footnote-section-div">Footnotes</h1>')
         }
     } catch (error) {
         console.error("Failed to generate footnotes.");
         console.error(error);
     }
-    
 
-    
-    
     try {
         document.getElementById('navigator').innerHTML = parseJSONToNav(GenerateJSON());
     } catch (error) {
         console.error("Failed to generate sidebar.");
         console.error(error);
     }
-    
+
     try {
         document.getElementsByClassName("sidebar")[0].classList.remove("bg-white");
     } catch (error) {
@@ -228,9 +203,11 @@ window.onload = function () {
         console.error(error);
     }
 
-    
-    
-    AddNewScriptTags();
-    changeFavicon(favicon_url);
-
+    try {
+        let theme = localStorage.getItem('theme');
+        SetTheme(theme);
+    } catch (error) {
+        console.error("Failed to load theme from local storage.");
+        console.error(error);
+    }
 };
